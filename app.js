82795487f41e4b97e9c4952185e5e6dab8adf129
Faizankhan147963 <faizankhan147963@gmail.com>
2025-01-25 10:13:47 +0530

    Replace bcrypt with bcryptjs

--- app.js
+++ app.js
@@ -20,9 +20,18 @@ app.get('/', (req, res) => {
 
 
 // Enable CORS for http://localhost:5173
+const allowedOrigins = ['http://localhost:5173', 'https://careerconnectin.netlify.app/'];
+
 app.use(cors({
-  origin: 'http://localhost:5173'
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
 }));
+
 
 app.use('/api/auth', authRoutes);  // Auth routes (Sign up and login)
 app.use('/api/users', userRoutes);  // User management routes (CRUD for users, Admin only)
